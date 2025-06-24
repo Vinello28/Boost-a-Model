d6482b6129f6fbc8b46bd0a6f515c1c6f4a09229
server <server@univpm.it>
2025-06-24 13:08:27 +0000

    better sender

--- src/run_sender.fish
+++ src/run_sender.fish
@@ -1,6 +1,15 @@
 #!/usr/bin/env fish
 source .env.fish
 echo "Starting BAM sender..."
+echo "SSH_USER and SSH_MACHINE are set to: $SSH_USER@$SSH_MACHINE via .env.fish"
+echo "REMEMBER: sender must be run on the machine with the camera connected, NOT IN THE REMOTE!!!"
+echo "NOTE: you will be prompted for a password, the prompt will probably be hidden somewhere in the terminal, so be sure to type it in and press Enter."
+read -p "Are you sure you run this on the machine with the camera? (y/N): " -n 1 -s confirm
+
+if test $confirm != 'y'
+    echo "\nExiting..."
+    exit 1
+end
 
 # ffmpeg -re -i /dev/video0 -f mpegts - | ssh -p $SSH_PORT $SSH_USER@$SSH_MACHINE 'docker exec -i bam-contener ffplay -i -'
 set TIME ( date +%Y-%m-%d_%H-%M-%S )
