f263d4100a8565e2ca927a072152b8f971898a9c
Gabriele Vianello <vianello.tech@gmail.com>
2025-06-28 15:49:27 +0200

    o la vittoria o tutti accoppati!

--- CNS-devel/buildandrun.sh
+++ CNS-devel/buildandrun.sh
@@ -1,7 +1,7 @@
-docker build . -t cns_sim
+docker build . -t cns_sim_container
 
 docker run --rm -t -d \
-    --name cns_sim \
+    --name cns_sim_container \
     --network="host" \
     -e DISPLAY=$DISPLAY \
     --mount src="$(pwd)",target=/root/,type=bind \
@@ -9,14 +9,14 @@ docker run --rm -t -d \
     -v $(pwd)/output_images:/workspace/output_images \
     -w /root \
     --runtime=nvidia \
-    --gpus "device=2" \
+    --gpus "device=1" \
     --privileged \
-    cns_sim
+    cns_sim_container
 
 # -v $(pwd):/workspace \
 # -v $(pwd)/checkpoints/:/root/checkpoints/ \
 
-docker exec -it cns_sim bash
+docker exec -it cns_sim_container bash
 clear
 read -p "Do you want to stop container? (y/N):" answer
 
